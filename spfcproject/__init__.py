706b7f10c067651923162d53d5317ab0e471d70f
Nicolas <104907066+nicolasandreos@users.noreply.github.com>
2025-03-04 16:46:46 -0300

    Update __init__.py

--- spfcproject/__init__.py
+++ spfcproject/__init__.py
@@ -8,11 +8,10 @@ import sqlalchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '76de173de227420f653cf3c10a713543'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:eGgDbswVysUqIgZbUDPIlEDTvBMrINcF@postgres.railway.internal:5432/railway'
-# if os.getenv('DATABASE_URL'):
-#     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-# else:
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetospfc.db'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetospfc.db'
 
 database = SQLAlchemy(app)
 criptografia = Bcrypt(app)
@@ -22,8 +21,8 @@ login_manager.login_message = 'Por favor para continuar, faça Login.'
 login_manager.login_message_category = 'alert-info'
 
 from spfcproject import models
-engine = sqlalchemy.create_engine("mysql+pymysql://user:pw@host/db", pool_pre_ping=True)
-# engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+# engine = sqlalchemy.create_engine("mysql+pymysql://user:pw@host/db", pool_pre_ping=True)
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
 if not inspector.has_table("usuario"):
     with app.app_context():
